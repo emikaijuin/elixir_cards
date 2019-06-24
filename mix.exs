dd57a52e430c800ed2602aaa5aeda08ec1c2d0a4
Emika Ijuin <emikaijuin@gmail.com>
2019-06-24 15:16:49 -0400

    update cards

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Cards.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~>0.18"}
+      {:ex_doc, "~> 0.12.0", only: :dev, runtime: false}
     ]
   end
 end
